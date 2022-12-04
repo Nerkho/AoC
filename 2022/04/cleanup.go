5b46556d2f52ea504750674b91427bd54a04e226
Nerkho <yannis.rachid@protonamil.ch>
2022-12-04 12:24:04 +0100

    AoC 2022: Day 4

---
+++ 2022/04/cleanup.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	fmt.Println(err)
+	return
+}
+
+func main() {
+	file, err := os.Open("input")
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var a int
+	var b int
+	for scanner.Scan() {
+		pair := strings.Split(scanner.Text(), ",")
+		if comparePairA(pair) {
+			a = a + 1
+		}
+		if comparePairB(pair) {
+			b = b + 1
+		}
+	}
+	fmt.Println("Part A :", a)
+	fmt.Println("Part B :", b)
+}
+
+func comparePairA(pair []string) bool {
+	var pairInPair bool
+	left := fullRange(pair[0])
+	right := fullRange(pair[1])
+	x := max(len(left), len(right))
+	if x == len(left) {
+		if contains(left, right[0]) && contains(left, right[len(right)-1]) {
+			pairInPair = true
+		}
+	}
+	if x == len(right) {
+		if contains(right, left[0]) && contains(right, left[len(left)-1]) {
+			pairInPair = true
+		}
+	}
+	return pairInPair
+}
+
+func comparePairB(pair []string) bool {
+	var pairInPair bool
+	left := fullRange(pair[0])
+	right := fullRange(pair[1])
+	x := max(len(left), len(right))
+	if x == len(left) {
+		pairInPair = overlap(left, right)
+	}
+	if x == len(right) {
+		pairInPair = overlap(right, left)
+	}
+	return pairInPair
+}
+
+func fullRange(oneSide string) []string {
+	var fullRange []string
+	fRange := strings.Split(oneSide, "-")
+	min, err := strconv.Atoi(fRange[0])
+	max, err := strconv.Atoi(fRange[1])
+	check(err)
+	for i := min; i < max+1; i++ {
+		fullRange = append(fullRange, strconv.Itoa(i))
+	}
+	return fullRange
+}
+
+func overlap(s, e []string) bool {
+	var b bool
+	for i := 0; i < len(e); i++ {
+		if contains(s, e[i]) {
+			b = true
+			break
+		}
+	}
+	return b
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func max(x, y int) int {
+	if y > x {
+		return y
+	}
+	return x
+}
