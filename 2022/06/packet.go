bfb8dc72926af6d837ba569058b980615705fca0
Nerkho <yannis.rachid@protonamil.ch>
2022-12-06 22:49:58 +0100

    AoC 2022: Day 6

---
+++ 2022/06/packet.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(err error) {
+	fmt.Println(err)
+	return
+}
+
+func main() {
+	file, err := os.Open("input")
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var x string
+	for scanner.Scan() {
+		x = scanner.Text()
+	}
+	a := common(x, 4)
+	b := common(x, 14)
+	fmt.Println("Part A :", a)
+	fmt.Println("Part B :", b)
+}
+
+func common(input string, length int) int {
+	numberChar := 0
+	for i := length - 1; i < len(input); i++ {
+		m := map[byte]int{}
+		for j := 0; j < length; j++ {
+			m[input[i-j]]++
+			fmt.Println(m)
+		}
+		if len(m) == length {
+			numberChar = i + 1
+			break
+		}
+	}
+	return numberChar
+}
